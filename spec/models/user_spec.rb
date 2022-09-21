3ab3568bdbf5e444a2825402eb4dd92a8a6d0f26
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-21 13:57:59 +0000

    test models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  include Devise: :Test: :IntegrationHelpers
+  include Devise::Test::IntegrationHelpers
   subject do
     User.new(name: 'test', email: 'example@email.com', password: '123456')
   end
