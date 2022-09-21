2b7da65c0d6c1f6c8d5c80af8b305c6c6839799e
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-09-21 14:50:43 +0000

    Add group controller actions

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -3,9 +3,30 @@
 class GroupController < ApplicationController # rubocop:todo Style/Documentation
   load_and_authorize_resource
 
-  def index; end
+  def index
+    @groups = current_user.groups
+  end
 
-  def new; end
+  def new
+    @user = current_user
+    @group = @user.groups.new
+  end
 
-  def create; end
+  def create
+    @user = current_user
+    @group = @user.groups.create(group_params)
+    if @group.save
+      flash[:notice] = 'Group created successfully'
+      redirect_to group_index_path
+    else
+      flash.now[:alert] = 'Group creation failed'
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
